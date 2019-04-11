791e646eb2af116506925ca57641bc43b0b85602
DG <d.gill.design@gmail.com>
2019-04-11 17:54:05 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log (string.toUpperCase())
+}
+logShout('hello')
+
+function logWhisper(string) {
+  console.log (string.toLowerCase())
+}
+logWhisper('HELLO')
+
+function sayHiToGrandma(string) {
+  return string.mixedCase()
+}
+sayHiToGrandma("i can't hear you")
